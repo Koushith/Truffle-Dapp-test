51f23597200110392efc65c6fbf49352a6cf1411
koushith <koushith97@gmail.com>
2020-07-13 17:39:49 +0530

    migrated

--- contracts/Hello.sol
+++ contracts/Hello.sol
@@ -1,40 +1,17 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract Hello {
-    //this will hold our gretings text
     string greeting;
 
     constructor() public {
-        // constructor to initilize the default greeting to hello
         greeting = "hello";
     }
 
-    function getGreeting() public view returns (string) {
-        // returns greeting
+    function getGreeting() public view returns (string memory) {
         return greeting;
     }
 
-    function setGreeting(string _greeting) public {
-        // user provided greeting
+    function setGreeting(string memory _greeting) public {
         greeting = _greeting;
     }
 }
-
-// pragma solidity >0.4.24;
-
-// contract HelloWorld {
-//     string saySomething;
-
-//     constructor() public {
-//         saySomething = "Hello World!";
-//     }
-
-//     function speak() public view returns (string itSays) {
-//         return saySomething;
-//     }
-
-//     function saySomethingElse(string newSaying) public returns (bool success) {
-//         saySomething = newSaying;
-//         return true;
-//     }
-// }
