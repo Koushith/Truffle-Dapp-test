c545b69004d932fb026286b04fd6126dc928388b
koushith <koushith97@gmail.com>
2020-07-18 03:30:19 +0530

    updated

--- truffle-config.js
+++ truffle-config.js
@@ -4,7 +4,7 @@ module.exports = {
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
+    //
     //
     development: {
       host: '127.0.0.1', // Localhost (default: none)
